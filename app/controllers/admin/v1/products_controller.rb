af1e1c30baab8a957c925731360e00cac281b8ad
manoel bertin <manoeltirj@gmail.com>
2023-01-10 13:19:53 -0300

    seventh commit

--- app/controllers/admin/v1/products_controller.rb
+++ app/controllers/admin/v1/products_controller.rb
@@ -1,44 +1,42 @@
 module Admin::V1
   class ProductsController < ApiController
-    before_action :load_product, only: %i(show update destroy)
+    before_action :load_product, only: %i(update destroy)
     
-  def index
-    @products = load_products
-  end
+    def index
+      @loading_service = Admin::ModelLoadingService.new(Product.all, searchable_params)
+      @loading_service.call
+    end
 
-  def create
-    run_service
-  rescue Admin::ProductSavingService::NotSavedProductError
-    render_error(fields: @saving_service.errors)
-  end
-  
-  def show; end
-  
-  def update
-    run_service
-  rescue Admin::ProductSavingService::NotSavedProductError
-    render_error(fields: @saving_service.errors)
-  end
-  
-  def destroy
-    @product.productable.destroy!
-    @product.destroy!
-  rescue ActiveRecord::RecordNotDestroyed
-    render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
-  end
+    def create
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
+    end
+
+    def update
+      run_service
+    rescue Admin::ProductSavingService::NotSavedProductError
+      render_error(fields: @saving_service.errors)
+    end
+
+    def destroy
+      @product.productable.destroy!
+      @product.destroy!
+    rescue ActiveRecord::RecordNotDestroyed
+      render_error(fields: @product.errors.messages.merge(@product.productable.errors.messages))
+    end
 
     private
+
+    def searchable_params
+      params.permit({ search: :name}, { order: {} }, :page, :length)
+    end
 
     def load_product
       @product = Product.find(params[:id])
     end
 
-    def load_products
-      permitted = params.permit({ search: :name }, { order: {} }, :page, :length)
-      Admin::ModelLoadingService.new(Product.all, permitted).call
-    end
-
-    def run_service(product = nil)
+    def run_service
       @saving_service = Admin::ProductSavingService.new(product_params.to_h, @product)
       @saving_service.call
       @product = @saving_service.product
@@ -47,19 +45,17 @@ module Admin::V1
 
     def product_params
       return {} unless params.has_key?(:product)
-      permitted_params = params.require(:product).permit(:id, :name, :description, :image, 
-                                                         :price, :productable,
-                                                         :status, category_ids: [])
+      permitted_params = params.require(:product).permit(:id, :name, :description, :image, :price, :productable)
       permitted_params.merge(productable_params)
     end
-    
+
     def productable_params
       productable_type = params[:product][:productable] || @product&.productable_type&.underscore
       return unless productable_type.present?
       productable_attributes = send("#{productable_type}_params")
       { productable_attributes: productable_attributes }
     end
-    
+
     def game_params
       params.require(:product).permit(:mode, :release_date, :developer, :system_requirement_id)
     end
--- app/views/admin/v1/products/index.json.jbuilder
+++ app/views/admin/v1/products/index.json.jbuilder
@@ -1,6 +1,10 @@
 json.products do
-  json.array! @products do |product|
+  json.array! @load_product.records do |product|
     json.partial! product
     json.partial! product.productable
   end
+end
+
+json.meta do
+  json.partial! 'shared/pagination', pagination: @loading_service.pagination
 end
